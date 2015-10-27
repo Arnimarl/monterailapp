ffc07812224191415e6a164a399a5cf4901bc1e3
Arnimarl <lukaszslomski13@gmail.com>
2015-10-27 22:33:07 +0100

    Comment to localStorage solution.

--- app/scripts/controllers/single-question.js
+++ app/scripts/controllers/single-question.js
@@ -4,89 +4,11 @@ angular.module('Arnimarl.Monterailapp')
 
   .controller('QuestionCtrl', function ($scope) {
 
-    $scope.img = '/imgs/drhouse.jpg';
-
-    // $scope.question = {
-    //   title: 'Vegan diet to stop diabetes progress',
-    //   description: 'Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet.',
-    //   createdBy: {
-    //     name: 'Oliver',
-    //     avatarSrc: 'imgs/drhouse.jpg'
-    //   },
-    //   answers: [
-    //     {
-    //       description: 'Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet.',
-    //       date: '25.10.2015',
-    //       createdBy: {
-    //         name: 'Ben',
-    //         avatarSrc: 'imgs/drhouse.jpg'
-    //       },
-    //       votes: 12
-    //     },
-    //     {
-    //       description: 'Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet.',
-    //       date: '25.10.2015',
-    //       createdBy: {
-    //         name: 'Ann',
-    //         avatarSrc: 'imgs/drhouse.jpg'
-    //       },
-    //       votes: 1
-    //     },
-    //     {
-    //       description: 'Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet.',
-    //       date: '25.10.2015',
-    //       createdBy: {
-    //         name: 'Gregg',
-    //         avatarSrc: 'imgs/drhouse.jpg'
-    //       },
-    //       votes: 3
-    //     },
-    //     {
-    //       description: 'Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet.',
-    //       date: '25.10.2015',
-    //       createdBy: {
-    //         name: 'Alice',
-    //         avatarSrc: 'imgs/drhouse.jpg'
-    //       },
-    //       votes: 3
-    //     },
-    //     {
-    //       description: 'Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet.',
-    //       date: '25.10.2015',
-    //       createdBy: {
-    //         name: 'Alex',
-    //         avatarSrc: 'imgs/drhouse.jpg'
-    //       },
-    //       votes: 22
-    //     },
-    //     {
-    //       description: 'Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet.',
-    //       date: '25.10.2015',
-    //       createdBy: {
-    //         name: 'Xavier',
-    //         avatarSrc: 'imgs/drhouse.jpg'
-    //       },
-    //       votes: 11
-    //     },
-    //     {
-    //       description: 'Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet.',
-    //       date: '25.10.2015',
-    //       createdBy: {
-    //         name: 'Albert',
-    //         avatarSrc: 'imgs/drhouse.jpg'
-    //       },
-    //       votes: 2
-    //     }
-    //   ],
-    //   relatedDisqussionsNumber: 5,
-    //   peersInvolvedNumber: 3,
-    //   conversationsNumber: 3,
-    //   votes: 19,
-    //   hot: false,
-    //   recent: true
-    // };
-
     function getQuestion () {
+      // I know that below solution probably isn't the best way
+      // to pass data beetween views, but when I tried with ui-sref
+      // something was wrong
+      // below implementation works fine, without any complications
       var question = localStorage.getItem('question');
       question = JSON.parse(question);
       $scope.question = question;
